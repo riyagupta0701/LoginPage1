92ae05f7083a93369692ca636aae4b2a5bce8808
riyagupta0701 <riyagupta2002@gmail.com>
2020-02-18 21:08:31 +0530

    Initial Commit

---
+++ LoginPage1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginPage1
+//
+//  Created by Riya Gupta on 18/02/20.
+//  Copyright © 2020 Riya Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
